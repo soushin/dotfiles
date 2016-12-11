976f112b70c3251abd5d9456c5702dee7d7754f0
nozawa_soushi <nozawa_soushi@cyberagent.co.jp>
2016-12-11 10:15:13 +0900

    add oh-my-zsh

--- setup.sh
+++ setup.sh
@@ -6,3 +6,6 @@ for file in ${DOT_FILES[@]}
 do
     ln -s $HOME/dotfiles/$file $HOME/$file
 done
+
+# install oh-my-zsh
+[ ! -d ~/.oh-my-zsh ] && git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
