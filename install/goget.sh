91a1875c48eeaa2b469a0b1896277669e8715406
soushin <n.soushi@gmail.com>
2021-12-20 18:00:45 +0900

    refactoring

--- install/goget.sh
+++ install/goget.sh
@@ -5,6 +5,6 @@ GO111MODULE=on go install github.com/golang/mock/gomock
 GO111MODULE=on go install github.com/golang/mock/mockgen
 GO111MODULE=on go install golang.org/x/tools/gopls@latest
 
-GO111MODULE=on go install -u github.com/saibing/bingo
+GO111MODULE=on go install github.com/saibing/bingo
 GO111MODULE=on go install golang.org/x/tools/cmd/goimports@latest
-GO111MODULE=on go install -u github.com/cweill/gotests/...
+GO111MODULE=on go install github.com/cweill/gotests/...
